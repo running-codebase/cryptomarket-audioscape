d6d765685a1763d7546355d738e2a43a36eb3ed6
running-codebase <info@traviscoulter.ca>
2017-02-05 21:02:39 +1100

    Different thunder files can be played based on how many deviation steps the particular trade is away from the mean. Constants for rain and thunder volume

--- rain_player.py
+++ rain_player.py
@@ -9,17 +9,21 @@ import pdb
 
 class RainPlayer():
 
-    def __init__(self, background, thunder):
+    def __init__(self, background, thunders):
         self.background = background
-        self.thunder = thunder
+        self.thunders = thunders
+        self.rain_vol = 0.3
+        self.thunder_vol = 0.2
         self.PAIR = "USDT_BTC"
         self.ONEDAY = 86400 
+        #self.PERIOD_LENGTH = 60
         self.PERIOD_LENGTH = 60
         self.TIMEOUT = 10
+        self.steps_per_std = 2
 
     def run(self):
         self._calculate_buckets()
-        self._play_sound(self.background, 0.3)
+        self._play_sound(self.background, self.rain_vol)
         while(True):
             start_time = time.time()
             orders = self._get_orders(self.PERIOD_LENGTH)
@@ -32,8 +36,8 @@ class RainPlayer():
     def _calculate_buckets(self):
         trades = self._get_orders(self.ONEDAY)
         self.mean = trades.mean()
-        deviation = trades.std() 
-        self.max = deviation *2 
+        self.std = trades.std() 
+        pdb.set_trace()
 
     def _get_orders(self, minutes):
         start_time = time.time() - minutes
@@ -41,6 +45,7 @@ class RainPlayer():
         try:
             temp = polo.marketTradeHist(self.PAIR, start_time, time.time())
             df = pd.DataFrame(temp)
+            pdb.set_trace()
             if len(df) > 0:
                 return pd.to_numeric(pd.Series(df["amount"]))
             else:
@@ -50,11 +55,16 @@ class RainPlayer():
         
 
     def _play_lightning(self, orders):
-        total = len(orders)
         for order in orders:
-            volume = order / self.max
-            self._play_sound(self.thunder, volume)
-            time.sleep(self.PERIOD_LENGTH /float(total))
+            step = int((order - self.mean) / (self.std / self.steps_per_std))
+            if step <= 0:
+                self._play_sound(self.thunders[0], self.thunder_vol)
+            elif step < len(self.thunders) -1:
+                self._play_sound(self.thunders[step], self.thunder_vol)
+            else: #no sound for this step
+                self._play_sound(self.thunders[-1], self.thunder_vol)
+                
+            time.sleep(self.PERIOD_LENGTH /float(len(orders)))
 
     def _play_sound(self, filename, volume):
         command = 'afplay {} -v {}'.format(filename, volume)
@@ -62,5 +72,9 @@ class RainPlayer():
 
 
 if __name__ == '__main__':
-    rp = RainPlayer('rain.mp3', 'thunder.mp3')
+    thunders = ['thunder.mp3',\
+                'recover.wav',\
+                'start.wav',\
+                'rest.wav']
+    rp = RainPlayer('rain.mp3', thunders)
     rp.run()
