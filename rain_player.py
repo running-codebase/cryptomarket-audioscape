f6fc1873ba044e14888d036d08976d42664d8a32
running-codebase <info@traviscoulter.ca>
2017-02-05 20:23:15 +1100

    Removed comments and blank spaces

--- rain_player.py
+++ rain_player.py
@@ -20,15 +20,11 @@ class RainPlayer():
     def run(self):
         self._calculate_buckets()
         self._play_sound(self.background, 0.3)
-        print "starting"
         while(True):
             start_time = time.time()
             orders = self._get_orders(self.PERIOD_LENGTH)
             if orders is not None:
-                print "orders"
                 self._play_lightning(orders)
-            else:
-                print "No orders"
             length = int(self.PERIOD_LENGTH - (time.time() - start_time))
             if length > 0:
                 time.sleep(length)
@@ -38,7 +34,6 @@ class RainPlayer():
         self.mean = trades.mean()
         deviation = trades.std() 
         self.max = deviation *2 
-        
 
     def _get_orders(self, minutes):
         start_time = time.time() - minutes
@@ -46,13 +41,11 @@ class RainPlayer():
         try:
             temp = polo.marketTradeHist(self.PAIR, start_time, time.time())
             df = pd.DataFrame(temp)
-            print df.head()
             if len(df) > 0:
                 return pd.to_numeric(pd.Series(df["amount"]))
             else:
                 return None
         except requests.exceptions.Timeout:
-            print "Timeout occurred"
             return None
         
 
@@ -61,17 +54,11 @@ class RainPlayer():
         for order in orders:
             volume = order / self.max
             self._play_sound(self.thunder, volume)
-            print str(volume)
             time.sleep(self.PERIOD_LENGTH /float(total))
-
-
 
     def _play_sound(self, filename, volume):
         command = 'afplay {} -v {}'.format(filename, volume)
-        print command
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-
-
 
 
 if __name__ == '__main__':
