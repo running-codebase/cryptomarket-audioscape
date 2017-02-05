d6d6618912efa04a797e7cd550ad4dac92eab69c
running-codebase <info@traviscoulter.ca>
2017-02-05 21:29:03 +1100

    trading pair as input argument

--- rain_player.py
+++ rain_player.py
@@ -9,14 +9,13 @@ from poloniex import Poloniex
 
 class RainPlayer():
 
-    def __init__(self, background, thunders):
+    def __init__(self, pair, background, thunders):
         self.background = background
         self.thunders = thunders
         self.rain_vol = 0.3
         self.thunder_vol = 0.2
-        self.PAIR = "USDT_BTC"
+        self.PAIR = pair 
         self.ONEDAY = 86400 
-        #self.PERIOD_LENGTH = 60
         self.PERIOD_LENGTH = 60
         self.TIMEOUT = 10
         self.steps_per_std = 2
@@ -71,10 +70,10 @@ class RainPlayer():
 
 if __name__ == '__main__':
 
-    if len(sys.argv) < 3:
-        print "usage python rain_player.py rain.mp3 thunder1.mp3 thunder2.mp3 ..."
+    if len(sys.argv) < 4:
+        print "usage python pair rain_player.py rain.mp3 thunder1.mp3 thunder2.mp3 ..."
         print "minimum of rain audio and one thunder audio required"
         exit()
     else:
-        rp = RainPlayer(sys.argv[1], sys.argv[2:])
+        rp = RainPlayer(sys.argv[1], sys.argv[2], sys.argv[3:])
         rp.run()
